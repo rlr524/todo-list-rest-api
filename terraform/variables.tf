7f06ac728b7979ec7a29b6b75e804ea230111a48
Rob Ranf <rob@emiyaconsulting.com>
2025-08-09 16:05:37 -0700

    removing tf

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "lambda_code_bucket" {
-  type        = string
-  description = "The S3 bucket in which the lambda source code will be stored"
-}
-
-variable "environment" {
-  type        = string
-  description = "The environment in which the application is being deployed"
-}
--- terraform/variables/dev.tfvars
+++
@@ -1,2 +0,0 @@
-environment        = "dev"
-lambda_code_bucket = "emiya-todo-lambda-source-code-dev"
