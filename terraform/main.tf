30d74762935e562ef5028c303752a816b5122b79
Rob Ranf <rob@emiyaconsulting.com>
2025-08-09 13:42:00 -0700

    adding aws api gateway in tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,7 @@ resource "aws_iam_role" "execution_role_for_lambda" {
 
 data "archive_file" "lambda_archive_file" {
   type        = "zip"
-  source_file = "../dist/*"
+  source_dir  = "../dist"
   output_path = "${path.root}/function.zip"
 }
 
@@ -53,4 +53,55 @@ resource "aws_lambda_function" "todo-list-rest-api" {
   }
 
   depends_on = [aws_s3_bucket.lambda_code_bucket, aws_iam_role.execution_role_for_lambda]
+}
+
+resource "aws_api_gateway_rest_api" "express_api_gateway" {
+  name = "EmiyaToDoExpressApiGateway"
+  description = "API Gateway for ToDo List API Express Lambda"
+}
+
+resource "aws_api_gateway_resource" "proxy_resource" {
+  rest_api_id = aws_api_gateway_rest_api.express_api_gateway.id
+  parent_id = aws_api_gateway_rest_api.express_api_gateway.root_resource_id
+  path_part = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy_method" {
+  rest_api_id = aws_api_gateway_rest_api.express_api_gateway.id
+  resource_id = aws_api_gateway_resource.proxy_resource.id
+  http_method = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.express_api_gateway.id
+  resource_id             = aws_api_gateway_method.proxy_method.resource_id
+  http_method             = aws_api_gateway_method.proxy_method.http_method
+  integration_http_method = "POST" # Always POST for Lambda proxy integration
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.todo-list-rest-api.invoke_arn
+}
+
+resource "aws_lambda_permission" "apigateway_permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.todo-list-rest-api.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.express_api_gateway.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_deployment" "api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.express_api_gateway.id
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.express_api_gateway.body))
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "prod_stage" {
+  deployment_id = aws_api_gateway_deployment.api_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.express_api_gateway.id
+  stage_name    = "prod"
 }
